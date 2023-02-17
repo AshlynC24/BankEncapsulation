8e53af4c890cf49822d6eb57cda310e162358a0d
Ashlyn Carmosino <ashlyncarmosino@Ashlyns-Air.lan>
2023-02-17 14:39:52 -0500

    bank account exercise

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace BankEncapsulation
+{
+	public class BankAccount
+	{
+		public BankAccount()
+		{
+		}
+		private double balance = 0;
+
+		//define a method that will accept a double
+		//and store that value in the balance field
+
+		public void Deposit(double amount) //encapsulation 
+		{
+			balance = amount;
+		}
+
+		//define a method named GetBalance that will
+		//return the amount stored in the balance feild
+
+		public double GetBalance()
+		{
+			return balance;
+		}
+		
+	}
+}
+
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //now allow the user to make a deposit in the bank account
+            //then they need to retrieve the balance 
+
+ 
+            var account = new BankAccount();
+            Console.WriteLine("Hello, how much would you like to deposit?");
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            account.Deposit(amountToDeposit);//this takes the amount and stores it into the balance field
+
+            Console.WriteLine($"Thank you, your balance is now: {account.GetBalance()}");
+            // now we write their new balance to the console 
+
         }
     }
 }
